b7f41ac3cf575e794a04479a78afd58b978b3316
hrybach-oleksiy <a.a.gribach@gmail.com>
2022-11-15 10:33:55 +0200

    feat: finish task 1-6

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,45 @@ function showHello(divName: string, name: string) {
     elt.innerText = `Hello from ${name}`;
 }
 
-const getAllBooks = () => {
-    const books = [
-        { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true },
-        { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: mfalse },
-        { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true },
-        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
+// ===================================== //
+enum Category { JavaScript, CSS, HTML, TypeScript, Angular }
+
+type Books = {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+}[];
+
+const getAllBooks = (): Books => {
+    const books: Books = [
+        { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JavaScript },
+        { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JavaScript },
+        { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JavaScript }
     ];
+
+    return books;
 };
+
+const logFirstAvailable = (books: Books): void => {
+    console.log(`Number of books ${books.length}`);
+    const firstAvailableBook = books.find(book => book.available === true)?.title;
+    // destructurisation
+    // const firstAvailableBook = books.find(({ available }) => available === true).title;
+    console.log(`First available book is ${firstAvailableBook}`);
+};
+
+logFirstAvailable(getAllBooks());
+
+const getBookTitlesByCategory = (bookCategory: Category): Array<string> => {
+    const books = getAllBooks();
+
+    const title = books.filter(book => book.category === bookCategory)
+        .map(book => book.title);
+
+    return title;
+};
+
+console.log(getBookTitlesByCategory(Category.JavaScript));
